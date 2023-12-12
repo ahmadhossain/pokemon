8b0756bdee5ae13d42144132463933d8c1b041da
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2023-12-12 10:31:20 +0600

    data persistant on header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -8,12 +8,29 @@ import cardImg from "../../public/cart.png";
 import logo from "../../public/logo.png";
 import Link from "next/link";
 import { useCart } from "@/Hooks/useCart";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const router = useRouter();
   const { isLogin, logout } = useUser();
   const { cart } = useCart();
+  const [cartObject, setCartObject] = useState(cart);
   const isHide = router.route === "/login";
+
+  console.log(cart);
+
+  useEffect(() => {
+    const cartData = localStorage.getItem("cart");
+    const cartObj = cartData ? JSON.parse(cartData) : null;
+
+    if (cartObj) {
+      setCartObject(cartObj);
+    }
+  }, [cart]);
+
+  useEffect(() => {
+    if (cart.length !== 0) localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <div className="w-full py-3 bg-gradient-to-b from-cyan-700  to-cyan-900 px-10 flex justify-between gap-5">
@@ -28,7 +45,7 @@ const Header = () => {
           <div className="flex relative">
             <Image width={30} src={cardImg} alt="Cart Image" />
             <p className="absolute right-0 -m-2 font-semibold text-red-300">
-              {cart.length > 0 && cart.length}
+              {cartObject.length > 0 && cartObject.length}
             </p>
           </div>
         </Link>
